14d21bfe0f2cec3f7affeb30cc7bca7aaf6aea71
Fideline <fideline_bzn@yahoo.com>
2022-06-29 16:22:03 +0200

    working xp leveling

--- xp.js
+++ xp.js
@@ -7,8 +7,13 @@ module.exports = async (message) => {
         let min = 1
         let max = 1
         return Math.floor(Math.random() * (max - min + 1)) + min;
-    };       // si c'est le bot qui envoie le message
-
+    }; 
+    function generateLevel() {
+        let min = 1
+        let max = 1
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };     
+     // si c'est le bot qui envoie le message
     if(message.author.bot) return;
     //si c'est pas un message privée
     if(message.channel.type === "dm") return;
@@ -17,13 +22,17 @@ module.exports = async (message) => {
     
     let sql = '';
 
-    
-
     if(result.length < 1) {
         sql = `INSERT INTO xp (user_id, xp_count) VALUES ('${message.author.id}', ${generateXp()})`
     } else {
         let xp_count = result[0].xp_count;
-        sql = `UPDATE xp SET xp_count = ${xp_count +generateXp()} WHERE user_id = '${message.author.id}'`;
+        let xp_level = result[0].xp_level;
+        if(xp_count < 4) {
+            sql = `UPDATE xp SET xp_count = ${xp_count +generateXp()} WHERE user_id = '${message.author.id}'`;
+        } else{
+            sql = `UPDATE xp SET xp_count = "0", xp_level = "${xp_level +generateXp()}"  WHERE user_id = '${message.author.id}'`;
+        }
+        
     }
 
     await mysql.executeQuery(sql);
